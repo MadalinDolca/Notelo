703fb464956c8810310c0d1f0037c7a8cefd8f1c
Mădălin <59412123+MadalinDolca@users.noreply.github.com>
2022-09-13 21:15:24 +0300

    Initial commit

---
+++ app/src/main/java/com/madalin/notelo/SplashScreenActivity.kt
@@ -0,0 +1,23 @@
+package com.madalin.notelo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Handler().postDelayed({
+            if (Firebase.auth.currentUser != null) {
+                startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+            } else {
+                startActivity(Intent(this@SplashScreenActivity, LoginActivity::class.java))
+            }
+            finish()
+        }, 500)
+    }
+}
