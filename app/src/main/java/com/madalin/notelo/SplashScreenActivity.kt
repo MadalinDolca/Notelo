3f119b0a27aa6543b167c369fba824ed1704911d
Mădălin <59412123+MadalinDolca@users.noreply.github.com>
2022-09-22 01:21:05 +0300

    Update SplashScreenActivity.kt

--- app/src/main/java/com/madalin/notelo/SplashScreenActivity.kt
+++ app/src/main/java/com/madalin/notelo/SplashScreenActivity.kt
@@ -1,23 +1,31 @@
 package com.madalin.notelo
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // checks if the current Firebase user is signed-in to start the MainActivity, otherwise starts LoginActivity
         Handler().postDelayed({
-            if (Firebase.auth.currentUser != null) {
+            auth = Firebase.auth
+            auth.currentUser?.getIdToken(true)
+
+            if (auth.currentUser != null) {
                 startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
             } else {
                 startActivity(Intent(this@SplashScreenActivity, LoginActivity::class.java))
             }
-            finish()
+            finish() // close the activity
         }, 500)
     }
 }
