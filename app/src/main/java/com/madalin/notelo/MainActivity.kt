5e2af0be276b610a05d891d5c325c422c05c20e6
Mădălin <59412123+MadalinDolca@users.noreply.github.com>
2022-09-22 01:27:52 +0300

    Binding

--- app/src/main/java/com/madalin/notelo/MainActivity.kt
+++ app/src/main/java/com/madalin/notelo/MainActivity.kt
@@ -2,10 +2,16 @@ package com.madalin.notelo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.madalin.notelo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root) // setContentView(R.layout.activity_main)
     }
 }
